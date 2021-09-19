e780c17a9f04d75f5ce51fe1051d7e66ef1dc70b
Prabhat <prabhat.namdharani@learner.manipal.edu>
2021-09-19 15:24:31 +0530

    Test version v1 Factorial, Sine and Cosine square

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+double factorial(double n){
+    if(n<=1){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+    }
+}
+double cos_square(double a){
+    return cos(a)*cos(a);
+}
+
+double sine_square(double a){
+    return sin(a)*sin(a);
+}
+
+int main() {
+   
+   return 0;
+}
+
+
+
+// double tan
